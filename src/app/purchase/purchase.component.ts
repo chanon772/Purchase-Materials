daa61c8b94c58d6802f8a651573c6b6983b551aa
bp5555b <chanon772@gmail.com>
2023-05-22 14:13:49 +0700

    Create Purchasing and Edit Component

---
+++ src/app/purchase/purchase.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-purchase',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './purchase.component.html',
+  styleUrls: ['./purchase.component.css']
+})
+export class PurchaseComponent {
+
+}
---
+++ src/app/purchasing/purchasing.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-purchasing',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './purchasing.component.html',
+  styleUrls: ['./purchasing.component.css']
+})
+export class PurchasingComponent {
+
+}
