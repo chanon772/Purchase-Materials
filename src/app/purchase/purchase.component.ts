368770667eafc61deac90f5bcba464e5100623f3
bp5553b <kk0870972149@gmail.com>
2023-05-22 14:59:25 +0700

    add routes and create Html purchase

--- src/app/purchase/purchase.component.ts
+++ src/app/purchase/purchase.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-purchase',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,PurchaseComponent,ReactiveFormsModule],
   templateUrl: './purchase.component.html',
   styleUrls: ['./purchase.component.css']
 })
